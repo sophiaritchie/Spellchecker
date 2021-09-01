b7d9d456aafa868a5188a93dedd5b41ff36f00e4
sophiaritchie <sophritchie@gmail.com>
2021-09-01 17:06:32 +1200

    Add form handlers

--- spellchecker/src/App.tsx
+++ spellchecker/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -6,12 +6,36 @@ import './App.css';
 import { Form, Container } from 'react-bootstrap';
 
 const App = () => {
+  const [word, setWord] = useState('');
+
+  function handleInput(e: ChangeEvent<HTMLInputElement>) {
+    console.log(e);
+    e.preventDefault();
+    setWord(e.currentTarget.value);
+  }
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const apiUrl = `http://localhost:3001?${word}`;
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
   return (
     <Container className="p-3">
-      <h1 className="header">Ogden's Basic English Spell Checker</h1>
-      <Form onSubmit={() => {}}>
-        <Form.Group className="mb-3" controlId="formGroupPassword">
-          <Form.Control size="lg" type="text" placeholder="Enter any word!" />
+      <h1 className="header">Ogden&apos;s Basic English Spell Checker</h1>
+      <Form
+        onSubmit={() => {
+          handleSubmit;
+        }}
+      >
+        <Form.Group className="mb-3" controlId="wordInput">
+          <Form.Control
+            size="lg"
+            type="text"
+            placeholder="Enter any word!"
+            onChange={() => handleInput}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
@@ -20,5 +44,4 @@ const App = () => {
     </Container>
   );
 };
-
 export default App;
