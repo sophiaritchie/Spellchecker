011eeb8cdd64221914e4bc8a458c129e4c822db8
sophiaritchie <sophritchie@gmail.com>
2021-09-01 13:34:04 +1200

    test husky

--- spellchecker/src/App.tsx
+++ spellchecker/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import asdsdf from 'assdfsdsdddda';
+import asdsdf from 'assdfsdsdsdfdsddda';
 
 function App() {
   return (
